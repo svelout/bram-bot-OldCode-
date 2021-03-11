ff4d7dce1519b02bb728f5ca6b01fd2020169e1f
bramerto-cmd <64208856+bramerto-cmd@users.noreply.github.com>
2021-03-11 21:38:17 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,5 +16,11 @@ async def on_ready():
 async def on_member_join(ctx):
     role = discord.utils.get(ctx.guild.roles, name='•member•')#•member•
     await ctx.add_roles(role)
+
+@bot.command(name='delete')
+async def on_message_delete(ctx):
+    guild = ctx.get_guild(817341710228521010)
+    delmsg = ctx.get_message({guild.message.id})
+    await ctx.message_delete(delmsg)
  
 bot.run('Nzc2NTMxOTc4OTcxMTE5NjQ2.X62Pww.Zzq1j2Z8LycA-W8n4cW99DsiFzU')
