b68978083a81c93c30b73f29f8cb04c4a4437eee
bramerto-cmd <64208856+bramerto-cmd@users.noreply.github.com>
2021-03-11 19:28:30 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,11 +15,6 @@ async def on_ready():
 @bot.event
 async def on_member_join(ctx):
     role = discord.utils.get(ctx.guild.roles, name='•member•')#•member•
-    guild = ctx.get_guild(817341710228521010)
-    channel = ctx.get_channel(817488304869474316)
     await ctx.add_roles(role)
-    await ctx.channel.send('Привет {member.mention}! Добро пожаловать на сервер редьюсеров! ')
-    await ctx.member.send('Совет от бота для тебя {member.name} , не забудь прочитать правила на сервере {guild.name} , иначе будет больно)')
-
-
+ 
 bot.run('Nzc2NTMxOTc4OTcxMTE5NjQ2.X62Pww.Zzq1j2Z8LycA-W8n4cW99DsiFzU')
