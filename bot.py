b359cb5e6b63a7485f2270a0b354869cd85459aa
bramerto-cmd <64208856+bramerto-cmd@users.noreply.github.com>
2021-03-11 22:34:49 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,13 +8,6 @@ intents.members = True
 
 bot = commands.Bot(command_prefix="?", intents=intents)
 
-class BotData:
-    def _init__(self):
-        self.welcome_channel = None
-        self.goodbye_channel = None
-
-botdata = BotData()
-
 @bot.event
 async def on_ready():
     print("BOT READY")
@@ -23,31 +16,5 @@ async def on_ready():
 async def on_member_join(ctx, member):
     role = discord.utils.get(ctx.guild.roles, name='•member•')#•member•
     await ctx.add_roles(role)
-    if botdata.welcome_channel.send != None:
-        await botdata.welcome_channel.send(f'Привет! {member.mention}! Добро пожаловать на редьюс сервер, хорошего пребывания')
-
-@bot.event
-async def on_member_remove(member):
-    if botdata.goodbye_channel != None:
-        await botdata.goodbye_channel.send(f" Прощай {member.mention}! Жаль, что ты ушел ")
-
-@bot.command()
-async def set_welcome_channel(ctx, channel_name=None):
-    if channel_name != None:
-        for channel in ctx.guild.channels:
-            if channel.name == channel_name:
-                botdata.welcome_channel = channel
-                await ctx.channel.send(f"Welcome channel = {channel.name}")
-                await channel.send("Это новый канал приветсвия")
-
-@bot.command()
-async def set_goodbye_channel(ctx, channel_name=None):
-    if channel_name != None:
-        for channel in ctx.guild.chanels:
-            if channel.name == channel_name:
-                botdata.goodbye_channel = channel
-                await ctx.channel.send(f"Goodbye channel = {channel.name}")
-                await channel.send("Это новый канал прощания")
-
- 
+  
 bot.run('Nzc2NTMxOTc4OTcxMTE5NjQ2.X62Pww.Zzq1j2Z8LycA-W8n4cW99DsiFzU')
