12a75bcad944a92ae17c2ad32dcb5b1568847887
bramerto-cmd <64208856+bramerto-cmd@users.noreply.github.com>
2021-03-11 16:20:04 +0300

    first edit

---
+++ bot.py
@@ -0,0 +1,22 @@
+import discord
+from discord.ext.commands import bot
+from discord.ext import commands
+from discord import member
+import os
+
+intents = discord.Intents.default()
+intents.members = True
+
+bot = commands.Bot(command_prefix=".", intents=intents)
+
+@bot.event
+async def on_ready():
+    print("BOT READY")
+
+@bot.event
+async def on_member_join(ctx):
+    role = discord.utils.get(ctx.guild.roles, name='•member•')#•member•
+    await ctx.add_roles(role)
+
+
+bot.run(os.environ['Nzc2NTMxOTc4OTcxMTE5NjQ2.X62Pww.Zzq1j2Z8LycA-W8n4cW99DsiFzU'])
