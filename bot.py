4a5b115894d528c962099feb382ba65c13e8a8e5
bramerto-cmd <64208856+bramerto-cmd@users.noreply.github.com>
2021-03-12 15:36:11 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -6,8 +6,7 @@ from discord import member
 intents = discord.Intents.default()
 intents.members = True
 
-bot = commands.Bot(command_prefix="?", intents=intents)
-
+bot = commands.Bot(command_prefix=".", intents=intents)
 
 @bot.event
 async def on_ready():
@@ -15,7 +14,7 @@ async def on_ready():
 
 @bot.event
 async def on_member_join(ctx):
-  role = discord.utils.get(ctx.guild.roles name='•member•')
-  await ctx.add_roles(role)
-  
+    role = discord.utils.get(ctx.guild.roles, name='•member•')#•member•
+    await ctx.add_roles(role)
+ 
 bot.run('Nzc2NTMxOTc4OTcxMTE5NjQ2.X62Pww.Zzq1j2Z8LycA-W8n4cW99DsiFzU')
