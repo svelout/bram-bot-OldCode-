e7fb0012dad89ae9519c78c7a244203ead8544c7
bramerto-cmd <64208856+bramerto-cmd@users.noreply.github.com>
2021-03-12 11:05:55 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -8,13 +8,27 @@ intents.members = True
 
 bot = commands.Bot(command_prefix="?", intents=intents)
 
+class BotData:
+	def_init_(self):
+		self.role=None
+
 @bot.event
 async def on_ready():
     print("BOT READY")
 
 @bot.event
 async def on_member_join(ctx):
-    role = discord.utils.get(ctx.guild.roles, name='•member•')#•member•
-    await ctx.add_roles(role)
+    if botdata.role_name=None:
+    	await botdata.add_roles(role_name)
+    
+@bot.command()
+async def set_role(ctx, role=None):
+  if role != None:
+  	for role in ctx.guild.roles:
+  		if role.name == role_name:
+  			role = guild.role
+  			await ctx.send('default role was: {role.name}')
+  else
+  	await ctx.send('Role not found')
   
 bot.run('Nzc2NTMxOTc4OTcxMTE5NjQ2.X62Pww.Zzq1j2Z8LycA-W8n4cW99DsiFzU')
