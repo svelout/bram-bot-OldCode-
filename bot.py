def6191a12ddd370f66ebbb36379d0421a9ec6cc
bramerto-cmd <64208856+bramerto-cmd@users.noreply.github.com>
2021-03-18 18:38:16 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext.commands import bot
 from discord.ext import commands
 from discord import member
+import time
 
 intents = discord.Intents.default()
 intents.members = True
@@ -17,26 +18,33 @@ async def on_member_join(ctx):
     role = discord.utils.get(ctx.guild.roles, name='•member•')#•member•
     await ctx.add_roles(role)
 
-@bot.command(name='kick')
-async def on_member_kick(ctx, member : discord.Member=None, *, reason=None):
-    await member.kick(reason=reason)
-    await ctx.send(f'{member.mention} был кикнут по причине:{reason}')
-    await member.send(f"Вы были кикнуты из сервера по причинк:{reason}")
-
 @bot.command()
-async def mute(ctx, member : discord.Member=None, *,reason=None, time=None):
-    role_mute = discord.utils.get(ctx.guild.roles, name='Muted')
+@commands.has_permissions(view_audit_log=True)
+async def kick(ctx, member : discord.Member,reason):
+    role_mute = discord.utils.get(ctx.guild.roles, id=822120846725218385)
     emb = discord.Embed(title='Mute', color=0xff0000)
     emb.add_field(name='Moderator',value=ctx.message.author.mention,inline=False)
     emb.add_field(name='Member',value=member.mention,inline=False)
     emb.add_field(name='Reason ',value=reason,inline=False)
     emb.add_field(name='Time',value=time,inline=False)
-    await member.add_roles(role_mute)
+    await member.kick()
+    await ctx.send(embed = emb)
+
+@bot.command()
+@commands.has_permissions(view_audit_log=True)
+async def mute(ctx, member : discord.Member,reason):
+    role_mute = discord.utils.get(ctx.guild.roles, id=822120846725218385)
+    emb = discord.Embed(title='Mute', color=0xff0000)
+    emb.add_field(name='Moderator',value=ctx.message.author.mention,inline=False)
+    emb.add_field(name='Member',value=member.mention,inline=False)
+    emb.add_field(name='Reason ',value=reason,inline=False)
+    emb.add_field(name='Time',value=time,inline=False)
+    await member.mute()
     await ctx.send(embed = emb)
 
 @bot.command()
 async def unmute(ctx, member):
-    await member.unmute
+    await member.unmute()
     await ctx.send(f'{member.mention} был размучен')
 
     
