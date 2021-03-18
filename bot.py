8e2ddd7512a40f0b8afa8fde6bf7aa445734cf25
bramerto-cmd <64208856+bramerto-cmd@users.noreply.github.com>
2021-03-18 18:21:19 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,5 +16,35 @@ async def on_ready():
 async def on_member_join(ctx):
     role = discord.utils.get(ctx.guild.roles, name='•member•')#•member•
     await ctx.add_roles(role)
+
+@bot.command(name='kick')
+async def on_member_kick(ctx, member : discord.Member=None, *, reason=None):
+    await member.kick(reason=reason)
+    await ctx.send(f'{member.mention} был кикнут по причине:{reason}')
+    await member.send(f"Вы были кикнуты из сервера по причинк:{reason}")
+
+@bot.command()
+async def mute(ctx, member : discord.Member=None, *,reason=None, time=None):
+    role2 = discord.utils.get(ctx.guild.roles, name='Muted')
+    emb = discord.Embed(title='Mute', color=0xff0000)
+    emb.add_field(name='Moderator',value=ctx.message.author.mention,inline=False)
+    emb.add_field(name='Member',value=member.mention,inline=False)
+    emb.add_field(name='Reason ',value=reason,inline=False)
+    emb.add_field(name='Time',value=time,inline=False)
+    await ctx.add_roles(role2)
+    await ctx.send(embed = emb)
+
+@bot.command()
+async def unmute(ctx, member):
+    await member.unmute
+    await ctx.send(f'{member.mention} был размучен')
+
+    
+    
+    
+   
+
+
+
  
 bot.run('Nzc2NTMxOTc4OTcxMTE5NjQ2.X62Pww.Zzq1j2Z8LycA-W8n4cW99DsiFzU')
