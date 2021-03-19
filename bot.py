9ba2efca65d0e314aa14e299dea88f8ce7755c7a
bramerto-cmd <64208856+bramerto-cmd@users.noreply.github.com>
2021-03-19 18:13:12 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,8 +2,8 @@ import discord
 from discord.ext.commands import bot
 from discord.ext import commands
 from discord import guild, member, user
-import time
 import asyncio
+from time import time
 
 intents = discord.Intents.default()
 intents.members = True
@@ -24,9 +24,9 @@ async def on_member_join(ctx):
 @commands.has_permissions(view_audit_log=True)
 async def kick(ctx, member : discord.Member,reason):
     emb = discord.Embed(title='Kick', color=0xff0000)
-    emb.add_field(name='Moderator',value=ctx.message.author.mention,inline=False)
-    emb.add_field(name='Member',value=member.mention,inline=False)
-    emb.add_field(name='Reason',value=reason,inline=False)
+    emb.add_field(name='Модератор',value=ctx.message.author.mention,inline=False)
+    emb.add_field(name='Участник',value=member.mention,inline=False)
+    emb.add_field(name='Причина',value=reason,inline=False)
     await member.kick()
     await ctx.send(embed = emb)
     await member.send(f'Вы были кикнуты с сервера по причине:{reason}')
@@ -36,10 +36,10 @@ async def kick(ctx, member : discord.Member,reason):
 async def mute(ctx, member : discord.Member,reason):
     rolemute = discord.utils.get(ctx.guild.roles, id=822120846725218385)
     emb = discord.Embed(title='Mute', color=0xff0000)
-    emb.add_field(name='Moderator',value=ctx.message.author.mention,inline=False)
-    emb.add_field(name='Member',value=member.mention,inline=False)
-    emb.add_field(name='Reason ',value=reason,inline=False)
-    emb.add_field(name='Time',value=time,inline=False)
+    emb.add_field(name='Модератор',value=ctx.message.author.mention,inline=False)
+    emb.add_field(name='Участник',value=member.mention,inline=False)
+    emb.add_field(name='Причина',value=reason,inline=False)
+    emb.add_field(name='Время',value=time,inline=False)
     await member.add_roles(rolemute)
     await ctx.send(embed = emb)
     await asyncio.sleep()
@@ -47,10 +47,21 @@ async def mute(ctx, member : discord.Member,reason):
 
 @bot.command()
 @commands.has_permissions(view_audit_log=True)
-async def unmute(ctx, member : discord.Member,reason):
-    rolemute = discord.utils.get(ctx.guild.roles, id=822120846725218385)
-    await member.remove_roles(rolemute)
+async def ban(ctx, member : discord.Member,reason):
+    emb = discord.Embed(title='Ban', color=0xff0000)
+    emb.add_field(name='Модератор',value=ctx.message.author.mention,inline=False)
+    emb.add_field(name='Участник',value=member.mention,inline=False)
+    emb.add_field(name='Причина',value=reason,inline=False)
+    emb.add_field(name='Время',value=time,inline=False)
+    await member.ban()
+    await ctx.send(embed=emb)
+    if time == None:
+        await member.send(f'Вы были забанены на сервере по причине:{reason}, навсегда')
+    else:
+        await member.send(f'Вы были забанены на сервере по причине:{reason}, время:{time}')
+    await asyncio.sleep()
+    await member.unban()
+    
 
 
- 
 bot.run('Nzc2NTMxOTc4OTcxMTE5NjQ2.X62Pww.Zzq1j2Z8LycA-W8n4cW99DsiFzU')
